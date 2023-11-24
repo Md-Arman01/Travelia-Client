39fedeaec697e38dcbe9537c35470841c5881e33
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2023-11-24 15:31:22 +0600

    added .env file and secret firebase confiq

--- src/firebase/firebase.confiq.js
+++ src/firebase/firebase.confiq.js
@@ -2,12 +2,12 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPrb31CKw-ccplDVx952U6ozakDA4t7bk",
-  authDomain: "travelia-db9f7.firebaseapp.com",
-  projectId: "travelia-db9f7",
-  storageBucket: "travelia-db9f7.appspot.com",
-  messagingSenderId: "523005017356",
-  appId: "1:523005017356:web:787c538ba661544b899d29",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
